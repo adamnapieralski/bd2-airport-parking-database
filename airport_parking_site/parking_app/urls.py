0688ac67242ff478cf0be706ed5504732a5ca8db
lukasz <l.kostrzewa12@gmail.com>
2020-06-11 20:16:15 +0200

    add blank pages

--- airport_parking_site/parking_app/urls.py
+++ airport_parking_site/parking_app/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    path('', include('parking_app.urls')),
+    path('', views.index, name='index'),
+    path('tickets', views.tickets, name='tickets'),
+    path('reporting', views.reporting, name='reporting'),
 ]
--- airport_parking_site/parking_app/views.py
+++ airport_parking_site/parking_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Index")
+
+def tickets(request):
+    return HttpResponse("Tickets")
+
+def reporting(request):
+    return HttpResponse("Reporting site.")
+
+    
