81165431b31ae6b9b8de8919c66a8c625344e822
lukasz <l.kostrzewa12@gmail.com>
2020-06-12 16:02:07 +0200

    add payment stats

--- airport_parking_site/parking_app/report.py
+++ airport_parking_site/parking_app/report.py
@@ -4,12 +4,26 @@ from django.conf import settings
 from django.http import HttpResponse
 
 def get_db_stats():
-    stats = []
-    stats.append(("Rezerwacje", models.Rezerwacja.objects.count()))
-    stats.append(("Bilety", models.Bilet.objects.count()))
-    stats.append(("Klienci", models.Klient.objects.count()))
-    stats.append(("Pojazdy", models.Pojazd.objects.count()))
-    return {'stats': stats}
+    general_stats = []
+    general_stats.append(("Rezerwacje", models.Rezerwacja.objects.count()))
+    general_stats.append(("Bilety", models.Bilet.objects.count()))
+    general_stats.append(("Klienci", models.Klient.objects.count()))
+    general_stats.append(("Pojazdy", models.Pojazd.objects.count()))
+
+    payment_stats = []
+    payments = models.Oplata.objects.values_list('kwota_ostateczna', flat=True)
+    discounts = models.Oplata.objects.filter(znizka__isnull = False).count()
+    penalties = models.Oplata.objects.filter(kara__isnull = False).count()
+
+    sum = 0
+    for p in payments:
+        sum += p       
+
+    payment_stats.append(("Łączny zysk", sum))
+    payment_stats.append(("Liczba przyznanych zniżek", discounts))
+    payment_stats.append(("Liczba nałożonych kar", penalties))
+
+    return {'general_stats': general_stats, 'payment_stats': payment_stats}
 
 
 def export_stats_to_csv():
@@ -25,5 +39,5 @@ def create_stats_csv(file_path):
     stats = get_db_stats()
     with open(file_path, 'w') as f:
         f.write('Statystyki parkingu\n')
-        for row in stats['stats']:
+        for row in stats['general_stats']:
             f.write(row[0] + ',' + str(row[1]) + '\n')
