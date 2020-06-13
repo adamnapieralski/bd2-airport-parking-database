e2787ac4ce41371f71feeff55f58e62c6b574c29
Wojtek <wrzesien.ww@gmail.com>
2020-06-14 00:21:51 +0200

    added login-register functionality

---
+++ airport_parking_site/parking_app/urls.py
@@ -0,0 +1,23 @@
+
+"""airport_parking_site URL Configuration
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='parking_app-home'),
+    path('home_ad/', views.home_admin, name='parking_app-home-admin'),
+    path('home_cl/', views.home_client, name='parking_app-home-client')
+]
--- airport_parking_site/parking_app/views.py
+++ airport_parking_site/parking_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'parking_app/home.html')
+
+
+def home_client(request):
+    return render(request, 'parking_app/home_cl.html')
+
+
+def home_admin(request):
+    return render(request, 'parking_app/home_ad.html')
